2458e912d6447122a08df31ffbb84c4b99970356
Alex <avillanu8@gmail.com>
2019-01-25 14:49:10 -0500

    Create joinmethod.js

---
+++ joinmethod.js
@@ -0,0 +1,8 @@
+function sentensify(str) {
+  // Add your code below this line
+  var returnStr = str.split(/\W/);
+  var joinStr = returnStr.join(" ");
+  return joinStr;  
+  
+  // Add your code above this line
+}
