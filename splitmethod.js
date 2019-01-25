a2971f7bd64eb762b81cfed60137cf8ee6150a8b
Alex <avillanu8@gmail.com>
2019-01-25 14:42:43 -0500

    Create splitmethod.js

---
+++ splitmethod.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Add your code below this line
+  var returnStr = str.split(/\W/);
+  return returnStr;
+  
+  // Add your code above this line
+}
+console.log(splitify("Hello World,I-am code"));
