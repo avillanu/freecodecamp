f4a12947a094742f086849c3d20c2eab1a83ce6b
Alex <avillanu8@gmail.com>
2019-01-26 16:48:24 -0500

    Create difftwoarrays.js

---
+++ difftwoarrays.js
@@ -0,0 +1,19 @@
+/*Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.
+*/
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  arr1.map(function(item){
+    if (arr2.indexOf(item) == -1)
+      {newArr.push(item);}     
+      });
+  arr2.map(function(item){
+    if (arr1.indexOf(item) == -1)
+      {newArr.push(item);}     
+      });    
+
+
+  return newArr;
+}
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
